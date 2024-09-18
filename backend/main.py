132b11ce4c78cf0ad77f43f019b0c22d883d6e2f
Nikita Zontov <zontovnikita@gmail.com>
2024-09-18 14:33:44 +0300

    Update

--- backend/main.py
+++ backend/main.py
@@ -17,9 +17,7 @@ app.add_middleware(
 
 SQLALCHEMY_DATABASE_URL = "postgresql://green:12345@go.itatmisis.ru/green_code"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
