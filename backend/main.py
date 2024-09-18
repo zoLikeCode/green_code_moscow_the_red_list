647ac38862fdd91f7d35af99b4e018e540e5f689
Nikita Zontov <zontovnikita@gmail.com>
2024-09-18 14:10:29 +0300

    upd

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://green:12345@go.itatmisis.ru/green_code"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-db = SessionLocal
-
 app = FastAPI()
 
 app.add_middleware(
@@ -24,6 +14,16 @@ app.add_middleware(
     allow_methods=['GET, POST, OPTIONS'],
     allow_headers=['*'],
 )
+
+SQLALCHEMY_DATABASE_URL = "postgresql://green:12345@go.itatmisis.ru/green_code"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db = SessionLocal
 
 @app.get('/start')
 async def start():
